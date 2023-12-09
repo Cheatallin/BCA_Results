2662b82e980bf525c096d1a1f902fe14ee2d32b6
Yiheng <development.henryquan@gmail.com>
2023-12-09 20:09:16 +1100

    write output to a log file

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 :: Replace Rscript.exe with the path to your Rscript.exe
-Rscript.exe .\BCA_Calculation_Template.R %*
+Rscript.exe .\BCA_Calculation_Template.R %* > output.log
 :: When drag & drop, wait for input so it's not closed immediately
 pause
